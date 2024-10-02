7cde2faf0211aa54c33fd97a1ffb32a2e884e041
Joepriestley <joeamonoo1114@gmail.com>
2024-10-02 12:21:07 +0100

    registering admin

--- gisportal/admin.py
+++ gisportal/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import(DRANEF,DPANEF,ZDTF,Region,Province,Commune,Forest,Canton,Groupe,Parcelle,Species,ParcelSpecies )
 
 # Register your models here.
+admin.site.register(DRANEF)
+admin.site.register(DPANEF)
+admin.site.register(ZDTF)
+admin.site.register(Region)
+admin.site.register(Province)
+admin.site.register(Commune)
+admin.site.register(Forest)
+admin.site.register(Canton)
+admin.site.register(Groupe)
+admin.site.register(Parcelle)
+admin.site.register(Species)
+admin.site.register(ParcelSpecies)
